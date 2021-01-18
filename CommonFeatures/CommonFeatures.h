38e0e6e6736c7091a5cbd99565b1224488757343
Sergio Bravo <s.bravo@globant.com>
2021-01-18 12:23:13 -0500

    Initial commit

---
+++ CommonFeatures/CommonFeatures.h
@@ -0,0 +1,18 @@
+//
+//  CommonFeatures.h
+//  CommonFeatures
+//
+//  Created by Sergio David Bravo Talero on 18/01/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CommonFeatures.
+FOUNDATION_EXPORT double CommonFeaturesVersionNumber;
+
+//! Project version string for CommonFeatures.
+FOUNDATION_EXPORT const unsigned char CommonFeaturesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CommonFeatures/PublicHeader.h>
+
+
---
+++ Injector/Injector.h
@@ -0,0 +1,18 @@
+//
+//  Injector.h
+//  Injector
+//
+//  Created by Sergio David Bravo Talero on 18/01/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Injector.
+FOUNDATION_EXPORT double InjectorVersionNumber;
+
+//! Project version string for Injector.
+FOUNDATION_EXPORT const unsigned char InjectorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Injector/PublicHeader.h>
+
+
