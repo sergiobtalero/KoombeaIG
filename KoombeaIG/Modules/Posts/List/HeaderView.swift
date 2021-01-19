ea29eecd1963d2f8779095f5a46c6884caf05d87
Sergio Bravo <s.bravo@globant.com>
2021-01-19 08:52:10 -0500

    Fixed bug with .yml

---
+++ KoombeaIG/Modules/Posts/List/HeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  HeaderView.swift
+//  KoombeaIG
+//
+//  Created by Sergio David Bravo Talero on 19/01/21.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        HeaderView()
+    }
+}
